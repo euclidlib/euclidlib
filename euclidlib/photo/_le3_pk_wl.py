6e5921012e618647f49b09126fa5080654a38663
Nicolas Tessore <n.tessore@ucl.ac.uk>
2024-04-13 22:28:26 +0100

    gh-13: update FITS format for LE3-PK-WL products (#12)
    * update FITS format of LE3-PK-WL
    
    * add unit tests
    
    * test all supported Python versions
    
    * remove black target-version

--- euclidlib/photo/_le3_pk_wl.py
+++ euclidlib/photo/_le3_pk_wl.py
@@ -1,27 +1,30 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
+import re
 from os import PathLike
-from typing import TYPE_CHECKING, Any, Tuple
+from typing import TYPE_CHECKING, Tuple, Union
 
 import fitsio  # type: ignore [import-not-found]
 import numpy as np
 from numpy.typing import NDArray
 
 if TYPE_CHECKING:
-    from typing import TypeAlias
+    from collections.abc import Iterator, Sequence
+    from typing import Any, TypeAlias
 
 
 # type alias
-TwoPointKey: TypeAlias = Tuple[str, str, int, int]
+_DictKey: "TypeAlias" = Union[str, int, Tuple["_DictKey", ...]]
 
 
 def toc_match(
-    key: tuple[Any, ...],
+    key: _DictKey,
     include: Sequence[tuple[Any, ...]] | None = None,
     exclude: Sequence[tuple[Any, ...]] | None = None,
 ) -> bool:
     """return whether a dict key matches include/exclude criteria"""
+    if not isinstance(key, tuple):
+        key = (key,)
     if include is not None:
         for pattern in include:
             if all(p is Ellipsis or p == k for p, k in zip(pattern, key)):
@@ -35,6 +38,18 @@ def toc_match(
     return True
 
 
+def _key_from_string(s: str) -> _DictKey:
+    """return key from string representation"""
+    keys = s.split(";")
+    if len(keys) > 1:
+        return tuple(map(_key_from_string, keys))
+    keys = keys[0].split(",")
+    if len(keys) > 1:
+        return tuple(map(_key_from_string, keys))
+    key = keys[0]
+    return int(key) if key.isdigit() else key
+
+
 def _read_metadata(hdu: fitsio.TableHDU) -> dict[str, Any]:
     """read array metadata from FITS HDU"""
     h = hdu.read_header()
@@ -45,14 +60,37 @@ def _read_metadata(hdu: fitsio.TableHDU) -> dict[str, Any]:
     return md
 
 
-def _read_twopoint(fits: fitsio.FITS, ext: str) -> NDArray[Any]:
-    """read two-point data from FITS"""
+def _read_twopoint(hdu: fitsio.TableHDU) -> NDArray[Any]:
+    """read two-point data from FITS HDU"""
     # read data from extension
-    arr: NDArray[Any] = fits[ext].read()
+    arr: NDArray[Any] = hdu.read()
     # attach metadata to dtype
-    dt = np.dtype(arr.dtype, metadata=_read_metadata(fits[ext]))
+    dt = np.dtype(arr.dtype, metadata=_read_metadata(hdu))
     # return a copy (unfortunately) with changed dtype
     return arr.astype(dt, casting="no", copy=True)
+
+
+def _iterfits(
+    path: str | PathLike[str],
+    tag: str,
+    include: Sequence[tuple[Any, ...]] | None = None,
+    exclude: Sequence[tuple[Any, ...]] | None = None,
+) -> Iterator[fitsio.TableHDU]:
+    """
+    Iterate over HDUs that correspond to *tag* and have valid keys.
+    """
+    with fitsio.FITS(path) as fits:
+        for hdu in fits:
+            if not re.match(f"^{tag}\\d+$", hdu.get_extname()):
+                continue
+            h = hdu.read_header()
+            s = h.get("DICTKEY")
+            if s is None:
+                continue
+            key = _key_from_string(s)
+            if not toc_match(key, include=include, exclude=exclude):
+                continue
+            yield key, hdu
 
 
 def angular_power_spectra(
@@ -60,31 +98,14 @@ def angular_power_spectra(
     *,
     include: Sequence[tuple[Any, ...]] | None = None,
     exclude: Sequence[tuple[Any, ...]] | None = None,
-) -> dict[TwoPointKey, NDArray[Any]]:
+) -> dict[_DictKey, NDArray[Any]]:
     """
     Read angular power spectra from a Euclid data product.
     """
 
-    # the returned set of cls
     cls = {}
-
-    # open the FITS file for reading
-    with fitsio.FITS(path) as fits:
-        # get the TOC from the FITS file
-        fits_toc = fits["CLTOC"].read()
-
-        # read every entry in the TOC, add it to the list, then read the cls
-        for entry in fits_toc:
-            ext, k1, k2, i1, i2 = entry[["EXT", "NAME1", "NAME2", "BIN1", "BIN2"]]
-
-            # skip if not selected
-            if not toc_match((k1, k2, i1, i2), include=include, exclude=exclude):
-                continue
-
-            # read the cl from the extension and store in set of cls
-            cls[k1, k2, i1, i2] = _read_twopoint(fits, ext)
-
-    # return the dictionary of cls
+    for key, hdu in _iterfits(path, "CL", include, exclude):
+        cls[key] = _read_twopoint(hdu)
     return cls
 
 
@@ -93,29 +114,12 @@ def mixing_matrices(
     *,
     include: Sequence[tuple[Any, ...]] | None = None,
     exclude: Sequence[tuple[Any, ...]] | None = None,
-) -> dict[TwoPointKey, NDArray[Any]]:
+) -> dict[_DictKey, NDArray[Any]]:
     """
     Read mixing matrices from a Euclid data product.
     """
 
-    # the returned set of mms
     mms = {}
-
-    # open the FITS file for reading
-    with fitsio.FITS(path) as fits:
-        # get the TOC from the FITS file
-        fits_toc = fits["MMTOC"].read()
-
-        # read every entry in the TOC, add it to the list, then read the mms
-        for entry in fits_toc:
-            ext, k1, k2, i1, i2 = entry[["EXT", "NAME1", "NAME2", "BIN1", "BIN2"]]
-
-            # skip if not selected
-            if not toc_match((k1, k2, i1, i2), include=include, exclude=exclude):
-                continue
-
-            # read the mixing matrix from the extension and store in set of mms
-            mms[k1, k2, i1, i2] = _read_twopoint(fits, ext)
-
-    # return the dictionary of mms
+    for key, hdu in _iterfits(path, "MM", include, exclude):
+        mms[key] = _read_twopoint(hdu)
     return mms
---
+++ tests/photo/test_le3_pk_wl.py
@@ -0,0 +1,43 @@
+import euclidlib as el
+
+
+def test_angular_power_spectra(data_path):
+    cls = el.photo.angular_power_spectra(data_path / "angular_power_spectra.fits")
+
+    expected_keys = [
+        ("P", "P", 0, 0),
+        ("P", "G_E", 0, 0),
+        ("P", "G_B", 0, 0),
+        ("G_E", "G_E", 0, 0),
+        ("G_B", "G_B", 0, 0),
+        ("G_E", "G_B", 0, 0),
+        ("P", "P", 0, 1),
+        ("P", "G_E", 0, 1),
+        ("P", "G_B", 0, 1),
+        ("G_E", "G_E", 0, 1),
+        ("G_B", "G_B", 0, 1),
+        ("G_E", "G_B", 0, 1),
+        ("P", "G_E", 1, 0),
+        ("P", "G_B", 1, 0),
+        ("G_E", "G_B", 1, 0),
+        ("P", "P", 1, 1),
+        ("P", "G_E", 1, 1),
+        ("P", "G_B", 1, 1),
+        ("G_E", "G_E", 1, 1),
+        ("G_B", "G_B", 1, 1),
+        ("G_E", "G_B", 1, 1),
+    ]
+
+    assert list(cls.keys()) == expected_keys
+
+
+def test_mixing_matrices(data_path):
+    mms = el.photo.mixing_matrices(data_path / "mixing_matrices.fits")
+
+    expected_keys = [
+        ("P", "P", 0, 1),
+        ("P", "G_E", 1, 2),
+        ("G_E", "G_E", 2, 3),
+    ]
+
+    assert list(mms.keys()) == expected_keys
