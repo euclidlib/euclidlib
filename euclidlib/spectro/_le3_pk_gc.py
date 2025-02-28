a7918b132c059da475ee08fe0251b93bae0b9073
Jacopo Salvalaggio <jacopo.salvalaggio@gmail.com>
2025-02-28 17:06:25 +0100

    Added function to read power spectrum from fits filename

--- euclidlib/spectro/_le3_pk_gc.py
+++ euclidlib/spectro/_le3_pk_gc.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Union
+from os import PathLike
 import numpy as np
 import fitsio
 from numpy.typing import NDArray
@@ -41,3 +42,21 @@ def _get_powerspectrum_header(hdu: fitsio.TableHDU) -> Dict[str, Any]:
     """
     pk_head: Dict[str, Any] = dict(hdu.read_header())
     return pk_head
+
+def read_powerspectrum(path: Union[str, PathLike[str]]) -> Tuple[Dict[str, Any], NDArray[Any]]:
+    """
+    Reads power spectrum from Euclid LE3-GC fits file
+
+    Parameters
+    ----------
+    filename : str | PathLike
+        path to the fits file
+
+    Returns
+    -------
+    header, data : Dict, NDArray
+    """
+    with fitsio.FITS(path) as fits_input:
+        header = _get_powerspectrum_header(fits_input[1])
+        data = _get_powerspectrum_data(fits_input[1])
+    return header, data
