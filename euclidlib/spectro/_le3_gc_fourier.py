eb682d66597281459cc74d99bd412be4156c76ea
Jacopo Salvalaggio <jacopo.salvalaggio@gmail.com>
2025-03-05 18:49:22 +0100

    Removed routines now present in utilily module

--- euclidlib/spectro/_le3_gc_fourier.py
+++ euclidlib/spectro/_le3_gc_fourier.py
@@ -4,44 +4,7 @@ from os import PathLike
 import numpy as np
 import fitsio
 from numpy.typing import NDArray
-
-def _get_powerspectrum_data(hdu: fitsio.TableHDU) -> NDArray[Any]:
-    """
-    Reads power spectrum data from fits file HDU
-
-    Parameters
-    ----------
-    hdu : fitsio.TableHDU
-        HDU from fits table (for any typical LE3-GC-PK file, the second in the list obtained when reading the fits file)
-
-    Returns
-    -------
-    pk_arr : NDArray
-        record array containing information on the power spectrum
-
-    Notes
-    -----
-    The columns in the pk record array are: "K", "K_EFF" (for now, it will just display the same values of "K"), "PKL" where "L" goes from "0" to "4" and "NUM_MOD".
-    """
-    pk_arr: NDArray[Any] = hdu.read()
-    return pk_arr
-
-def _get_powerspectrum_header(hdu: fitsio.TableHDU) -> Dict[str, Any]:
-    """
-    Reads power spectrum header from fits file HDU
-
-    Parameters
-    ----------
-    hdu : fitsio.TableHDU
-        HDU from fits table (for any typical LE3-GC-PK file, the second in the list obtained when reading the fits file)
-
-    Returns
-    -------
-    pk_head : Dict
-        dictionary containing all header entries
-    """
-    pk_head: Dict[str, Any] = dict(hdu.read_header())
-    return pk_head
+from ._utils import _get_hdu_header, _get_hdu_data
 
 def _read_powerspectrum(path: Union[str, PathLike[str]]) -> Tuple[Dict[str, Any], NDArray[Any]]:
     """
