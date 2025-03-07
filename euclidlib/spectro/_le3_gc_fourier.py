8a1cd9be059a7f6bd5fbff5590c453b14d3e3997
Jacopo Salvalaggio <jacopo.salvalaggio@gmail.com>
2025-03-07 10:56:13 +0100

    Reworked pk reading routine adding support for pk in bk files

--- euclidlib/spectro/_le3_gc_fourier.py
+++ euclidlib/spectro/_le3_gc_fourier.py
@@ -6,9 +6,9 @@ import fitsio
 from numpy.typing import NDArray
 from ._utils import _get_hdu_header, _get_hdu_data
 
-def _read_powerspectrum(path: Union[str, PathLike[str]]) -> Tuple[Dict[str, Any], NDArray[Any]]:
+def _read_power_spectrum(path: Union[str, PathLike[str]]) -> Tuple[Dict[str, Any], NDArray[Any]]:
     """
-    Reads power spectrum from Euclid LE3-GC fits file
+    Reads power spectrum from Euclid LE3-GC fits file, returning both header and data
 
     Parameters
     ----------
@@ -20,6 +20,15 @@ def _read_powerspectrum(path: Union[str, PathLike[str]]) -> Tuple[Dict[str, Any]
     header, data : Dict, NDArray
     """
     with fitsio.FITS(path) as fits_input:
-        header = _get_powerspectrum_header(fits_input[1])
-        data = _get_powerspectrum_data(fits_input[1])
+        header = _get_hdu_header(fits_input[1])
+        if header["EXTNAME"] == "SPECTRUM":
+            data = _get_hdu_data(fits_input[1])
+        # If Bispectrum file provided, retrieve pk from next hdu (yet to be tested!)
+        elif header["EXTNAME"] == "BISPECTRUM":
+            header = _get_hdu_header(fits_input[2])
+            data = _get_hdu_data(fits_input[2])
+        else:
+            raise ValueError(
+                "Invalid fits file provided, cannot locate Power Spectrum data."
+            )
     return header, data
