b3d0f55f746a086985a2eb6b851f990f0b5aada2
Guadalupe Canas-Herrera <guadalupe.canasherrera@esa.int>
2025-05-27 15:36:51 +0200

    added method to __init__

--- euclidlib/photo/harmonic_space/__init__.py
+++ euclidlib/photo/harmonic_space/__init__.py
@@ -1,4 +1,4 @@
 # photo/harmonic_space/__init__.py
-from ._le3_pk_wl import angular_power_spectra, mixing_matrices
+from ._le3_pk_wl import angular_power_spectra, mixing_matrices, covariance_matrices
 
-__all__ = ["angular_power_spectra", "mixing_matrices"]
+__all__ = ["angular_power_spectra", "mixing_matrices", "covariance_matrices"]
