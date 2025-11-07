7eb1a0e96c54d3220d5325de665dc4286ab6e0c1
Jacopo Salvalaggio <jacopo.salvalaggio@gmail.com>
2025-11-07 16:32:24 +0100

    Implemented basic attribute structure of Result dataclass

---
+++ euclidlib/spectro/_datamodel.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from dataclass import dataclass
+from numpy.typing import NDArray
+from typing import Any, Dict
+
+@dataclass(frozen=True)
+class Result:
+    """
+    Output datamodel for read LE3 GC PK measurements, containing information on the header and the power spectrum multipoles.
+
+    Attributes
+    ----------
+    k : ndarray
+        centers of k bins
+    k_eff : ndarray
+        effective values of k bins
+    mode_number : ndarray
+        number of modes in each k bin
+    p : dict[ndarray]
+        multipoles of the power spectrum (keys are integer l's from 0 to 4)
+    header : dict
+        fits header of the original measurement file
+    """
+    k: NDArray[Any]
+    k_eff: NDArray[Any]
+    mode_number: NDArray[Any]
+    p: Dict[int, NDArray[Any]]
+    header: Dict[str, Any]
