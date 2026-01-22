21eb25e32a8db208f563041b17da0d032762f7b3
gcanasherrera <canasherrera@strw.leidenuniv.nl>
2026-01-22 11:43:21 +0100

    Passing pre-commit, pytest, new release of cosmolib

--- euclidlib/le3/pk_gc.py
+++ euclidlib/le3/pk_gc.py
@@ -1,12 +1,25 @@
 from __future__ import annotations
-from warnings import warn
-from typing import Any, Dict, Tuple, Union
-from os import PathLike
-import fitsio  # type: ignore [import-not-found]
-import os
-from numpy.typing import NDArray
 
+from warnings import warn
+from os import PathLike
+import os
+import fitsio  # type: ignore [import-not-found]
+
+from typing import Optional, cast
+
+from numpy.typing import NDArray
 from cosmolib.data import PowerSpectrum  # type: ignore [import-not-found]
+
+TYPE_CHECKING = True
+if TYPE_CHECKING:
+    from typing import Any, Dict, Tuple, Union
+
+    try:
+        from typing import TypeAlias
+    except ImportError:
+        from typing_extensions import TypeAlias
+
+    _DictKey: TypeAlias = Union[str, int, tuple["_DictKey", ...]]
 
 
 def _verify_input_file(
@@ -14,30 +27,17 @@ def _verify_input_file(
 ) -> None:
     """
     Verifies that input file is a compatible LE3-GC fits file
-
-    Parameters
-    ----------
-    path : str | PathLike
-        path to the (hopefully) fits file
-    check_extra_hdu : bool
-        verify file contains information on two different hdu (e.g. bispectrum and power spectrum)
-
-    Raises
-    ------
-    TypeError
-        if `path` is not a valid string or PathLike object
-    FileNotFoundError
-        if file does not exist
-    ValueError
-        if file is not a fits file or is it a fits file with an incompatible structure
     """
     hdul_target_length = 3 if check_extra_hdu else 2
+
     if not any((isinstance(path, str), isinstance(path, PathLike))):
         raise TypeError(
             "Provided fits file name must be a string or a PathLike object."
         )
+
     if not os.path.isfile(path):
-        raise FileNotFoundError("Could not find file '{}'.".format(str(path)))
+        raise FileNotFoundError(f"Could not find file '{path}'.")
+
     try:
         with fitsio.FITS(path) as fits_input:
             assert len(fits_input) > hdul_target_length - 1
@@ -55,16 +55,6 @@ def _verify_input_file(
 def _get_hdu_header(hdu: fitsio.TableHDU) -> Dict[str, Any]:
     """
     Reads header from fits file HDU
-
-    Parameters
-    ----------
-    hdu : fitsio.TableHDU
-        HDU from fits table
-
-    Returns
-    -------
-    head : Dict
-        dictionary containing all header entries
     """
     head: Dict[str, Any] = dict(hdu.read_header())
     return head
@@ -73,16 +63,6 @@ def _get_hdu_header(hdu: fitsio.TableHDU) -> Dict[str, Any]:
 def _get_hdu_data(hdu: fitsio.TableHDU) -> NDArray[Any]:
     """
     Reads data from fits file HDU
-
-    Parameters
-    ----------
-    hdu : fitsio.TableHDU
-        HDU from fits table
-
-    Returns
-    -------
-    arr : NDArray
-        record array containing information stored in the hdu
     """
     arr: NDArray[Any] = hdu.read()
     return arr
@@ -92,31 +72,23 @@ def _read_power_spectrum(
     path: Union[str, PathLike[str]],
 ) -> Tuple[Dict[str, Any], NDArray[Any]]:
     """
-    Reads power spectrum from Euclid LE3-GC fits file, returning both header and data
-
-    Parameters
-    ----------
-    filename : str | PathLike
-        path to the fits file
-
-    Returns
-    -------
-    header, data : Dict, NDArray
+    Reads power spectrum from Euclid LE3-GC fits file
     """
-    # Multiply-used error message
     pk_not_found_message = (
         "Invalid fits file provided, cannot locate power spectrum data."
     )
-    # File quality check
+
     _verify_input_file(path)
-    # Read file and verify it contains information on the pk
+
     with fitsio.FITS(path) as fits_input:
         header = _get_hdu_header(fits_input[1])
+
         if "SPECTRUM" not in header["EXTNAME"]:
             raise ValueError(pk_not_found_message)
-        elif header["EXTNAME"] == "SPECTRUM":
+
+        if header["EXTNAME"] == "SPECTRUM":
             data = _get_hdu_data(fits_input[1])
-        # If a bispectrum file was provided, retrieve pk from next hdu and warn the user
+
         elif header["EXTNAME"] == "BISPECTRUM":
             warn(
                 "\n\033[0;31m[!]\033[0m This looks to be a bispectrum file. "
@@ -127,34 +99,54 @@ def _read_power_spectrum(
             if header["EXTNAME"] != "SPECTRUM":
                 raise ValueError(pk_not_found_message)
             data = _get_hdu_data(fits_input[2])
+
         else:
             raise ValueError(pk_not_found_message)
+
     return header, data
 
 
-def power_spectrum(path: Union[str, PathLike[str]]) -> PowerSpectrum:
+def power_spectrum(
+    path: Union[str, PathLike[str]],
+) -> dict[_DictKey, PowerSpectrum]:
     """
     Returns power spectrum data in the cloe-compatible euclidlib data format
-
-    Parameters
-    ----------
-    path : str | PathLike
-        path to the pk file
-
-    Returns
-    -------
-    pk_result : PowerSpectrum
-        class containing the pk data
     """
     header, data = _read_power_spectrum(path)
-    pk_result = PowerSpectrum(
-        data["K"],
-        data["K_EFF"],
-        data["NUM_MOD"],
-        {ell: data["PK{}".format(str(ell))] for ell in range(5)},
-        0,  # Fiducial cosmology is not stored in the fits file
-        0,  # redshift_eff is not stored in the fits file
-        1.0 / header["SN_VALUE"],
-        header["SN_VALUE"],
-    )
-    return pk_result
+
+    fidual_cosmology = {
+        "OMEGA_M": header["OMEGA_M"],
+        "OMEGA_R": header["OMEGA_R"],
+        "OMEGA_B": header["OMEGA_B"],
+        "OMEGA_V": header["OMEGA_V"],
+        "OMEGA_K": header["OMEGA_K"],
+        "HUBBLE": header["HUBBLE"],
+        "INDEX_N": header["INDEX_N"],
+        "SIGMA_8": header["SIGMA_8"],
+        "W_STATE": header["W_STATE"],
+        "N_EFF": header["N_EFF"],
+        "T_CMB": header["T_CMB"],
+    }
+
+    # Explicit annotation required for mypy
+    results: dict[_DictKey, Optional[PowerSpectrum]] = {}
+
+    # Create all keys (auto + cross)
+    for i in range(5):
+        for j in range(5):
+            results[("SPE", "SPE", i, j)] = None
+
+    # Fill only auto-correlations
+    for i in range(5):
+        results[("SPE", "SPE", i, i)] = PowerSpectrum(
+            data["K"],
+            data["K_EFF"],
+            data["NUM_MOD"],
+            data[f"PK{i}"],
+            fidual_cosmology,
+            0,  # redshift_eff is not stored
+            1.0 / header["SN_VALUE"],
+            header["SN_VALUE"],
+        )
+
+    return cast(dict[_DictKey, PowerSpectrum], results)
