e0e6600cf08fad2ada5db0f0b8843963da02c3e6
gcanasherrera <canasherrera@strw.leidenuniv.nl>
2026-03-02 16:42:10 +0100

    changed name to index

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@ from sphinx.application import Sphinx
 import os
 
 import sys
-
 sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
