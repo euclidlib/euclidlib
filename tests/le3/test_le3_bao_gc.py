4cd700fd9be85a73474e83de9e995b7af144eec3
chiara.moretti1989@gmail.com <chiara.moretti1989@gmail.com>
2026-03-24 19:08:38 +0100

    Added unit tests for BAO covariance

--- tests/le3/test_le3_bao_gc.py
+++ tests/le3/test_le3_bao_gc.py
@@ -3,9 +3,17 @@ import euclidlib as el
 
 def test_bao_gc_with_dummy_data(data_path):
     # Load the data
-    data = el.le3.bao_gc.BAO_alphas(data_path / "EUC_LE3_BAO_ALPHAS_Z1.0.fits")
+    data = el.le3.bao_gc.BAO_alphas(data_path / "EUC_BAO_ALPHAS_Z1.0.fits")
 
     assert data is not None, "Data should not be None"
     assert isinstance(data, dict), "Data should be a dictionary"
     assert ("SPE", "SPE", 0, 0) in data, "Data should contain ('SPE', 'SPE', 0, 0)"
     assert data[("SPE", "SPE", 0, 0)] is not None, "BAO alphas should not be None"
+
+def test_bao_gc_covariance(data_path):
+    data = el.le3.bao_gc.BAO_alphas_covariance(data_path / "EUC_BAO_COV_Z1.0.fits")
+
+    assert data is not None, "Data should not be None"
+    assert isinstance(data, dict), "Data should be a dictionary"
+    assert ("SPE", "SPE", 0, 0) in data, "Data should contain ('SPE', 'SPE', 0, 0)"
+    assert data[("SPE", "SPE", 0, 0)] is not None, "BAO covariance should not be None"
